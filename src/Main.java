15f329d8aa981751f79676b9448ce1eae92d6501
emkreimer <eduardakreimer@gmail.com>
2024-11-05 17:48:33 -0300

    solved exercises

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import view.AccountView;
+import view.BallView;
+import view.RectangleView;
+
+public class Main {
+    public static void main(String[] args) {
+        RectangleView.main(args);
+        AccountView.main(args);
+        BallView.main(args);
+    }
+}
